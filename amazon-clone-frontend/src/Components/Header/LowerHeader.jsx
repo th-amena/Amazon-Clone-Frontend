634bad5e1583a222277f3d6d03ecf2d852becd52
th-amena <hewan.tom@gmail.com>
2024-09-27 16:43:55 -0400

    new commit header reverted

--- amazon-clone-frontend/src/Components/Header/LowerHeader.jsx
+++ amazon-clone-frontend/src/Components/Header/LowerHeader.jsx
@@ -10,13 +10,12 @@ function LowerHeader() {
                 < MenuIcon  />
                 <p>All</p>
             </li>
-
             <li>Same-Day Delivery</li>
             <li>Medical Care</li>
             <li>Keep Shopping For</li>
             <li>Prime Video</li>
             <li>Amazon Bascis</li>
-            {/* <li>Today's Deals</li>
+            <li>Today's Deals</li>
             <li>Best Sellers</li>
             <li>Toy & Games</li>
             <li>Groceries</li>
@@ -24,7 +23,7 @@ function LowerHeader() {
             <li>Buy Again</li>
             <li>Shop By Interest</li>
             <li>Household, Health & Baby Care</li>
-            <li>Livestreams</li> */}
+            <li>Livestreams</li>
             <li>Subscribe & Save</li>
          </ul>
       </div>
